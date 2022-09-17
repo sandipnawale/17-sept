e597325c7581227959e68235dd914a4d3f390cfb
Dell <Dell@DESKTOP-EGOPRNC>
2022-09-17 08:35:20 +0530

    commited...

---
+++ src/git/Gitproject.java
@@ -0,0 +1,53 @@
+package git;
+
+public class Gitproject {
+	
+	
+	
+	@Test
+	public void home()
+	{
+		System.out.println("Home page of application");
+	}
+	
+	@Test
+	public void dashBoard()
+	{
+		System.out.println("Dashboard of application");
+	}
+	
+	@Test
+	public void logout()
+	{
+		System.out.println("Logout from the application");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
